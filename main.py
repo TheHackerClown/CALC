1cef2f62bc34ca9bd1101f5b62cd2dbac60ba2fb
Dhruv Pratap Singh <81506692+TheHackerClown@users.noreply.github.com>
2022-01-07 06:14:10 +0000

    updated files to folder

--- main.py
+++ main.py
@@ -12,23 +12,23 @@ print('What do you want:')
 print('1. Currency Convertor \n2. Divider Which says everything \n3. Arithematic Calculator \n4. Temprature Convertor\n5. Speed Convertor \n6. Factorial Finder\n7. Distance Formula Calculator\n8. Statistics Calculator\n9. Factor Finder')
 choice = input('Enter Your Choice [1/2/3/4/5/6/7/8/9]- ')
 if choice == '1':
-    exec(open("currencyCalc.py").read())
+    exec(open("files/currencyCalc.py").read())
 elif choice == '2':
-    exec(open("opendivider.py").read())
+    exec(open("files/opendivider.py").read())
 elif choice == '3':
-    exec(open("mathsCalc.py").read())
+    exec(open("files/mathsCalc.py").read())
 elif choice == '4':
-    exec(open("tempCalc.py").read())
+    exec(open("files/tempCalc.py").read())
 elif choice == '5':
-    exec(open("speedCalc.py").read())
+    exec(open("files/speedCalc.py").read())
 elif choice == '6':
 	num = int(input('Enter the number: '))
 	print('Factorial: ',factorial(num))
 elif choice == '7':
-  exec(open("distCalc.py").read())
+  exec(open("files/distCalc.py").read())
 elif choice == '8':
-  exec(open("statsCalc.py").read())
+  exec(open("files/statsCalc.py").read())
 elif choice == '9':
-  exec(open("factorFinder.py").read())
+  exec(open("files/factorFinder.py").read())
 else:
     print('Thanks for using me. \n Credits- Dhruv Pratap Singh,@TheHackerClown,ME-@dhruvpratap(both are my accounts)')
