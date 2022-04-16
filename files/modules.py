a107094f887d24e8d33e582a97eb3ab14d9e2fff
Dhruv Pratap Singh <81506692+TheHackerClown@users.noreply.github.com>
2022-04-16 14:28:07 +0530

    red blue green

---
+++ files/modules.py
@@ -0,0 +1,7 @@
+def tryclear():
+    import os
+    try:
+        os.system('cls')
+    except:
+        os.system('clear')
+
--- main.py
+++ main.py
@@ -1,61 +1,58 @@
 import math
-import requests
-import json
 import pyfiglet
+from termcolor import colored
 import os
 import time
-os.system('clear')
+from files import modules
+modules.tryclear()
 #os.system('cls')
-print(pyfiglet.figlet_format("C", font = "isometric2" ))
+print(colored(pyfiglet.figlet_format("C", font = "isometric1" ), 'red'))
 time.sleep(1)
-os.system('clear')
-print(pyfiglet.figlet_format("CA", font = "isometric1" ))
+modules.tryclear()
+print(colored(pyfiglet.figlet_format("CA", font = "isometric1" ), 'red'))
 time.sleep(1)
-os.system('clear')
-print(pyfiglet.figlet_format("CAL", font = "isometric2" ))
+modules.tryclear()
+print(colored(pyfiglet.figlet_format("CAL", font = "isometric1" ), 'red'))
 time.sleep(1)
-os.system('clear')
-print(pyfiglet.figlet_format("CALC", font = "isometric1" ))
+modules.tryclear()
+print(colored(pyfiglet.figlet_format("CALC", font = "isometric1" ), 'red'))
 time.sleep(1)
-os.system('clear')
-print(pyfiglet.figlet_format("CALC", font = "isometric1" ))
+modules.tryclear()
+print(colored(pyfiglet.figlet_format("CALC", font = "isometric1" ), 'cyan'))
 
-print('I am a ...... Everything you want as Calculator [Made VERY EASILY]')
-print('What do you want:')
-print("1. Currency Convertor \n2. Divider Which says everything \n3. Simple Calculator \n4. Temprature Convertor\n5. Speed Convertor \n6. Factorial Finder\n7. Distance Formula Calculator\n8. Statistics Calculator\n9. Factor Finder\n10. Area Of Triangle [Heron's Formula]\n11. Quadratic Formula\n12. LCM and HCF Finder")
-choice = input('Enter Your Choice [1/2/3/4/5/6/7/8/9/10/11/12]- ')
+print(colored('I am a can do everything that listed below :\n','red'))
+print(colored("\n[1. Currency Convertor ,2. Simple Calculator ,3. Temprature Convertor ,4. Speed Convertor ,5. Factorial Finder ,6. Distance Formula Calculator ,7. Statistics Calculator ,8. Factor Finder ,9. Area Of Triangle [Heron's Formula,Normal Formula] ,10. Quadratic Formula ,11. LCM and HCF Finder]\n\n",'red'))
+choice = input(colored('\nTest My Skills [1/2/3/4/5/6/7/8/9/10/11]- ','green'))
 if choice == '1':
     exec(open("files/currencyCalc.py").read())
 elif choice == '2':
-    exec(open("files/opendivider.py").read())
-elif choice == '3':
-    os.system('clear')
+    modules.tryclear()
     #os.system('cls')
     print(pyfiglet.figlet_format("CALC", font = "3-d" ))
     print('------------------------------------------')
     toeval = input('Enter the Equation : ')
     print(eval(toeval))
-elif choice == '4':
+elif choice == '3':
     exec(open("files/tempCalc.py").read())
-elif choice == '5':
+elif choice == '4':
     exec(open("files/speedCalc.py").read())
-elif choice == '6':
-  os.system('clear')
+elif choice == '5':
+  modules.tryclear()
   print(pyfiglet.figlet_format('Factorial', font='slant'))
   print('')
   factorial = math.factorial(int(input('Enter the number: ')))
   print(f'Factorial : {factorial}')
-elif choice == '7':
+elif choice == '6':
   exec(open("files/distCalc.py").read())
-elif choice == '8':
+elif choice == '7':
   exec(open("files/statsCalc.py").read())
-elif choice == '9':
+elif choice == '8':
   exec(open("files/factorFinder.py").read())
-elif choice == '10':
+elif choice == '9':
   exec(open("files/heronsform.py").read())
-elif choice == '11':
+elif choice == '10':
   exec(open("files/quadform.py").read())
-elif choice == '12':
+elif choice == '11':
   exec(open('files/lcmnhcf.py').read())
 else:
-    print('Thanks for using me. \n Credits- Dhruv Pratap Singh,@TheHackerClown')
+    print('Thanks for taking a whole knowledge-full bucket of calculation for my little big brain calculator.')
